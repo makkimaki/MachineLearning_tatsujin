1c9c2ce8a28bda8f4686b6ac61253f26f33b01aa
makkimaki <shellingford-93.4.2@hotmail.co.jp>
2020-04-20 21:33:46 +0900

    Added get_stats method.

--- datashell2.py
+++ datashell2.py
@@ -11,3 +11,6 @@ class DataShell_second(object):
     def rename_column(self, column_name, new_column_name):
         self.data_as_csv.columns = self.data_as_csv.columns.str.replace(column_name, new_column_name)
 
+    def get_stats(self):
+        # Return a description data_as_csv
+        return self.data_as_csv.describe()
